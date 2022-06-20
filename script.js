3c47d2f31a2d91dbe315d32f7b22efcebd28f775
emilia1814 <104419556+emilia1814@users.noreply.github.com>
2022-06-20 09:27:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+        question: "What is the capital of India?",
+        a: "Delhi",
+        b: "Mumbai",
+        c: "Chennai",
+        d: "Kolkata",
+        correct: 'a'
+    }, {
+        question: "What is the capital of USA?",
+        a: "New Delhi",
+        b: "Mumbai",
+        c: "Washington",
+        d: "Kolkata",
+        correct: 'c'
+    }, {
+        question: "What is the capital of Australia?",
+        a: "Rome",
+        b: "Sydney",
+        c: "Canberra",
+        d: "Queensland",
+        correct: 'b'
+    }, {
+        question: "Who is the Prime Minister of India?",
+        a: "Narendra Modi",
+        b: "Rahul Gandhi",
+        c: "Sonia Gandhi",
+        d: "Manmohan Singh",
+        correct: 'a'
+    }, {
+        question: "Who is the Prime Minister of Netherlands?",
+        a: "Donald Trump",
+        b: "Mark Rutte",
+        c: "Nigel Farage",
+        d: "Jos von Woude",
+        correct: 'b'
+    }
+];
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
+
+
